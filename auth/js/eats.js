7efe50594432a3b2e8ed524159fbc93a55ae538e
vsoch <vsochat@stanford.edu>
2016-09-01 13:57:57 -0700

    adding google map location to close #3

--- auth/js/eats.js
+++ auth/js/eats.js
@@ -20,7 +20,7 @@ map = new GMaps({
 });
 
 var image = '../img/favicon/apple-icon-57x57.png';
-var marker = map.addMarker({
+var gil = map.addMarker({
 	lat: lat,
 	lng: long,
 	icon: image,
@@ -81,8 +81,68 @@ map.setStyle("map_style");
 $("#location").val(lat +", "+ long);
 
 // Add event listener to fire a function when the marker is moved
-marker.addListener('dragend', function(evt) {
-    var lat = evt.latLng.lat().toFixed(3);
-    var long = evt.latLng.lng().toFixed(3);
-    $("#location").val(lat +", "+ long);
+gil.addListener('dragend', function(evt) {
+    console.log("hello! Have you ever seen a Gilfish? I'm a coffee cup with glasses!");
+});
+
+// Add link to restaurant selection input
+var address_input = (document.getElementById('address-input'));
+
+var types = document.getElementById('type-selector');
+
+var autocomplete = new google.maps.places.Autocomplete(address_input);
+autocomplete.bindTo('bounds', map.map);
+
+var infowindow = new google.maps.InfoWindow();
+var marker = new google.maps.Marker({
+    map: map.map,
+    anchorPoint: new google.maps.Point(0, -29),
+    icon: image
+});
+
+autocomplete.addListener('place_changed', function() {
+    infowindow.close();
+    marker.setVisible(false);
+    var place = autocomplete.getPlace();
+    if (!place.geometry) {
+        window.alert("Autocomplete's returned place contains no geometry");
+        return;
+    }
+
+    // If the place has a geometry, then present it on a map.
+    if (place.geometry.viewport) {
+        map.map.fitBounds(place.geometry.viewport);
+    } else {
+        map.map.setCenter(place.geometry.location);
+        map.map.setZoom(17);  // Why 17? Because it looks good.
+    }
+
+    marker.setIcon(/** @type {google.maps.Icon} */({
+        url: image,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(35, 35)
+    }));
+
+    // Update the hidden field to the location
+    var lat = place.geometry.location.lat();
+    var long = place.geometry.location.lng();
+    var location = lat + ", " + long;
+    $("#location").val(location);
+
+    marker.setPosition(place.geometry.location);
+    marker.setVisible(true);
+
+    var address = '';
+    if (place.address_components) {
+        address = [
+        (place.address_components[0] && place.address_components[0].short_name || ''),
+        (place.address_components[1] && place.address_components[1].short_name || ''),
+        (place.address_components[2] && place.address_components[2].short_name || '')
+        ].join(' ');
+    }
+
+    infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+    infowindow.open(map.map, marker);
 });
